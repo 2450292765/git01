9bd598a4cfa0866549e4a7050e7b0bfbeb262c12
JiaZq <59287424+2450292765@users.noreply.github.com>
2021-09-24 22:44:54 +0800

    Update test.java
    1

--- test.java
+++ test.java
@@ -1,8 +1,13 @@
 public class test{
+	
+	public test(){
+		System.out.println("起飞");
+	}
 
 	public static void main(String[] args){
 
 		System.out.println("芜湖");
+		new test();
 		System.out.println("123");
 }
 }
